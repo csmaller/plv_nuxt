d8f92c47d48ff53ed8fc4566f57e8aee7af05634
corey <csmaller@elumatherapy.com>
2023-11-26 16:46:45 -0700

    history

---
+++ pages/History.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { data: historyQuery } = await useAsyncData('history', () => {
+  return queryContent('home')
+    .where({ title: { $eq: 'history' } })
+    .findOne();
+});
+</script>
+
+<template>
+  <div v-if="historyQuery">
+    <LayoutContainer
+      :image="historyQuery?.image"
+      :position="historyQuery?.image_position"
+      :header="historyQuery?.header"
+    >
+      <ContentRenderer :value="historyQuery">
+        <ContentRendererMarkdown :value="historyQuery" />
+      </ContentRenderer>
+    </LayoutContainer>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
