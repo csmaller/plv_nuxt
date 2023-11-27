3a2d4418b87e79f3280b3b59e462cb28707c0645
corey <csmaller@elumatherapy.com>
2023-11-26 17:00:22 -0700

    orchard

---
+++ pages/Orchard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { data: orchardQuery } = await useAsyncData('orchard', () => {
+  return queryContent('home')
+    .where({ title: { $eq: 'orchard' } })
+    .findOne();
+});
+</script>
+
+<template>
+  <div v-if="orchardQuery">
+    <LayoutContainer
+      :image="orchardQuery?.image"
+      :position="orchardQuery?.image_position"
+      :header="orchardQuery?.header"
+    >
+      <ContentRenderer :value="orchardQuery">
+        <ContentRendererMarkdown :value="orchardQuery" />
+      </ContentRenderer>
+    </LayoutContainer>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
