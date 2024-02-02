a247c15570fd67e51cdc6d725065385e58d4b018
corey <csmaller@elumatherapy.com>
2024-02-02 11:23:47 -0700

    socials

--- components/Nav/NavDropdown.vue
+++ components/Nav/NavDropdown.vue
@@ -19,17 +19,17 @@ const props = withDefaults(defineProps<PropsInterface>(), {
 const { header, links, url, isMobile } = toRefs(props);
 
 const toggleMenu = (shouldShowMenu: boolean) => {
+  console.log(isMobile.value);
   if (!isMobile.value) showMenu.value = shouldShowMenu;
 };
 
-const showMenu = ref(isMobile ? true : false);
-console.log(showMenu.value);
+const showMenu = ref(isMobile.value ? true : false);
 </script>
 
 <template>
-  <div class="nav align-content-center p-0" @mouseover="toggleMenu(true)" @mouseleave="toggleMenu(false)">
+  <div class="nav align-content-center p-0 pb-3" @mouseover="toggleMenu(true)" @mouseleave="toggleMenu(false)">
     <NuxtLink class="nav-header flex w-full justify-content-center" :to="url">{{ header }}</NuxtLink>
-    <div v-if="showMenu" class="dropdown-menu flex flex-column z-100">
+    <div v-if="showMenu" class="dropdown-menu flex flex-column z-100 pb-3">
       <div class="link-container justify-content-end text-center p-0" v-for="link in links" :key="link.label">
         <NuxtLink class="w-full h-full p-0" :to="link.url">{{ link.label }}</NuxtLink>
       </div>
