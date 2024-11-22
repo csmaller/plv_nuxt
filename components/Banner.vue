cb31cfc0ac47fc235b341a73f13e4241f907268f
corey <csmaller@elumatherapy.com>
2024-11-21 20:51:55 -0700

    banner dynamic

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
-import images from '@/content/json/main.json';
+const { data: bannerQuery } = await useAsyncData('news', () => {
+  return queryContent('news')
+    .where({ title: { $eq: 'banner' } })
+    .find();
+});
+
+const isInDateRange = (start: string, end: string) => {
+  const today = new Date();
+  const bannerStart = new Date(start);
+  const bannerEnd = new Date(end);
+
+  return bannerStart <= today && bannerEnd >= today ? true : false;
+};
 </script>
 
 <template>
-  <div class="w-full flex justify-content-center my-5">
-    <ImageLink src="/img/banner2.png" url="/visit#tasting" />
+  <div
+    v-if="bannerQuery"
+    v-for="banner in bannerQuery"
+    :key="banner.start"
+    class="w-full flex justify-content-center my-5 px-5"
+  >
+    <img :src="banner.image" v-if="isInDateRange(banner.start, banner.end)" />
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+img {
+  padding: 6px;
+  margin-bottom: 5px;
+}
+</style>
